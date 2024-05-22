16f4f889eae41d4215d8d5b8b92a2d6ab14b49cb
Mateusz Pietrowiak <matas8@protonmail.com>
2024-05-22 18:33:43 +0200

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/bcd14c8f-f70a-4659-b4fa-d4cd656eafed-jlxq4b.jpg",
@@ -12,11 +13,18 @@ const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="flex flex-wrap gap-4">
-      {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-        <div key={image.id} className="w-48">
+      {posts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+      ))}
+      {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+        <div key={image.id + "-" + index} className="w-48">
           <img src={image.url} alt="image" />
         </div>
       ))}
