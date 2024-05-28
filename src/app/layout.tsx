f6a431ba5b8813dd609940bf5f188e431208a945
Mateusz Pietrowiak <matas8@protonmail.com>
2024-05-28 14:07:31 +0200

    add toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
 import { GeistSans } from "geist/font/sans";
+import { Toaster } from "~/components/ui/sonner";
 
 export const metadata = {
   title: "T3 Gallery",
@@ -32,6 +33,7 @@ export default function RootLayout({
           </div>
           {modal}
           <div id="modal-root" />
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
