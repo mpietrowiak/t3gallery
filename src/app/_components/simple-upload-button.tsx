aa73b612a7ed8390d003f8a425e7ae95766027d4
Mateusz Pietrowiak <matas8@protonmail.com>
2024-05-29 14:51:14 +0200

    add ratelimit

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -90,6 +90,11 @@ export function SimpleUploadButton() {
         },
       );
     },
+    onUploadError(error) {
+      posthog.capture("upload_error", { error });
+      toast.dismiss("upload-begin");
+      toast.error(`Upload failed! ${error.message}`);
+    },
     onClientUploadComplete() {
       toast.dismiss("upload-begin");
       toast("Upload complete!");
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,15 +2,19 @@ import { auth } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/server";
 import { UploadThingError } from "uploadthing/server";
 import { db } from "~/server/db";
+import { ratelimit } from "~/server/db/ratelimit";
 import { images } from "~/server/db/schema";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB" , maxFileCount: 4 } })
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 4 } })
     .middleware(async ({ req }) => {
       const user = auth();
       if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      const { success } = await ratelimit.limit(user.userId);
+      if (!success) throw new UploadThingError("Rate limited");
 
       return {
         userId: user.userId,
